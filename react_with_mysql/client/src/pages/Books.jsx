77b4174245e5092da77c0a43c73cb2ea86c1b2d1
ZhafranT <zhaftosa@gmail.com>
2023-02-06 21:47:05 +0700

    add cors 26:41

--- react_with_mysql/client/src/pages/Books.jsx
+++ react_with_mysql/client/src/pages/Books.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Books = () => {
-  return (
-    <div>Books</div>
-  )
-}
+   const [books, setBooks] = useState([]);
 
-export default Books
+   useEffect(() => {
+      const fetchAllBooks = async () => {
+         try {
+            const res = await axios.get('http://localhost:8800/books');
+            setBooks(res.data);
+         } catch (err) {
+            console.log(err);
+         }
+      };
+      fetchAllBooks();
+   }, []);
+
+   return (
+      <div>
+         <h1>all books</h1>
+         <div className="books">
+            {books.map((book) => (
+               <div className="book" key={book.id}>
+                  {book.cover && <img src={book.cover} alt="" />}
+                  <h2>{book.title}</h2>
+                  <p>{book.desc}</p>
+                  {/* <span>{book.price}</span> */}
+               </div>
+            ))}
+         </div>
+      </div>
+   );
+};
+
+export default Books;
